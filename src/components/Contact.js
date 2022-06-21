02d2c4bcb6e437d195ce8391ef21179977dcaf2c
emyekah <leadload6@gmail.com>
2022-06-22 00:29:52 +0700

    reactjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import '../App.css';
 
-const Contact = () => {
+const Contact = (prop) => {
     return (
-        <></>
+        <>
+        <div className='item'>
+            <div className='aside'>
+                <img src={prop.data.photo} alt={prop.data.name} className='image'></img>
+            </div>
+            <div className='section'>
+                <div className='nama'>{prop.data.name}</div>
+                <div className='phone-email'>{prop.data.phone}<br></br>
+                {prop.data.email}
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import '../App.css';
 
 const Header = () => {
     return (
-        <></>
+        <>
+        <div>
+            <div className='header'>
+                <h2>
+                    Call a friend
+                </h2>
+                <h4>
+                    Your friend contact app
+                </h4>
+            </div>
+        </div>
+        </>
     )
 }
 
